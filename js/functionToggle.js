b87946f21b57871882adc2f47731768114f36446
prestig9110 <prestig9110@gmail.com>
2021-04-05 20:47:40 +0300

    fix toggle

--- js/functionToggle.js
+++ js/functionToggle.js
@@ -2,3 +2,8 @@ function toggle() {
   const toggleButton = document.getElementById("links");
   toggleButton.classList.toggle("active");
 }
+
+// const toggle = document.getElementById('toggle');
+// toggle.onclick = function(){
+//   toggle.classList.toggle('active');
+//   }
