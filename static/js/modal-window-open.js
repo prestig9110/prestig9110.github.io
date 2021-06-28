eacff23175e0461d81fe71347a21228370cf06c1
prestig9110 <prestig9110@gmail.com>
2021-06-28 17:48:48 +0300

    gallery

---
+++ static/js/modal-window-open.js
@@ -0,0 +1,56 @@
+const openModal0001 = document.getElementById("openModal0001");
+const openModal0002 = document.getElementById("openModal0002");
+const openModal0003 = document.getElementById("openModal0003");
+const openModal0004 = document.getElementById("openModal0004");
+const openModal0005 = document.getElementById("openModal0005");
+const openModal0006 = document.getElementById("openModal0006");
+const openModal0007 = document.getElementById("openModal0007");
+const openModal0008 = document.getElementById("openModal0008");
+const openModal0009 = document.getElementById("openModal0009");
+const openModal0010 = document.getElementById("openModal0010");
+
+const modalContainer0001 = document.getElementById("modalContainer0001");
+const modalContainer0002 = document.getElementById("modalContainer0002");
+const modalContainer0003 = document.getElementById("modalContainer0003");
+const modalContainer0004 = document.getElementById("modalContainer0004");
+const modalContainer0005 = document.getElementById("modalContainer0005");
+const modalContainer0006 = document.getElementById("modalContainer0006");
+const modalContainer0007 = document.getElementById("modalContainer0007");
+const modalContainer0008 = document.getElementById("modalContainer0008");
+const modalContainer0009 = document.getElementById("modalContainer0009");
+const modalContainer0010 = document.getElementById("modalContainer0010");
+
+const close0001 = document.getElementById("close0001");
+const close0002 = document.getElementById("close0002");
+const close0003 = document.getElementById("close0003");
+const close0004 = document.getElementById("close0004");
+const close0005 = document.getElementById("close0005");
+const close0006 = document.getElementById("close0006");
+const close0007 = document.getElementById("close0007");
+const close0008 = document.getElementById("close0008");
+const close0009 = document.getElementById("close0009");
+const close0010 = document.getElementById("close0010");
+
+
+
+openModal0001.addEventListener("click", () => { modalContainer0001.classList.add("show"); });
+openModal0002.addEventListener("click", () => { modalContainer0002.classList.add("show"); });
+openModal0003.addEventListener("click", () => { modalContainer0003.classList.add("show"); });
+openModal0004.addEventListener("click", () => { modalContainer0004.classList.add("show"); });
+openModal0005.addEventListener("click", () => { modalContainer0005.classList.add("show"); });
+openModal0006.addEventListener("click", () => { modalContainer0006.classList.add("show"); });
+openModal0007.addEventListener("click", () => { modalContainer0007.classList.add("show"); });
+openModal0008.addEventListener("click", () => { modalContainer0008.classList.add("show"); });
+openModal0009.addEventListener("click", () => { modalContainer0009.classList.add("show"); });
+openModal0010.addEventListener("click", () => { modalContainer0010.classList.add("show"); });
+
+close0001.addEventListener("click", () => { modalContainer0001.classList.remove("show"); });
+close0002.addEventListener("click", () => { modalContainer0002.classList.remove("show"); });
+close0003.addEventListener("click", () => { modalContainer0003.classList.remove("show"); });
+close0004.addEventListener("click", () => { modalContainer0004.classList.remove("show"); });
+close0005.addEventListener("click", () => { modalContainer0005.classList.remove("show"); });
+close0006.addEventListener("click", () => { modalContainer0006.classList.remove("show"); });
+close0007.addEventListener("click", () => { modalContainer0007.classList.remove("show"); });
+close0008.addEventListener("click", () => { modalContainer0008.classList.remove("show"); });
+close0009.addEventListener("click", () => { modalContainer0009.classList.remove("show"); });
+close0010.addEventListener("click", () => { modalContainer0010.classList.remove("show"); });
