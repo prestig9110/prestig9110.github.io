930db68db3267beaf1d12142e763c81c64c1bd53
prestig9110 <prestig9110@gmail.com>
2021-08-11 17:36:00 +0300

    all fix phone

--- static/js/preloader.js
+++ static/js/preloader.js
@@ -1,4 +1,6 @@
 window.onload = function () {
-  let preloader = document.getElementById("preloader");
-  preloader.style.display = " none";
+  setTimeout(() => {
+    let preloader = document.getElementById("preloader");
+    preloader.style.display = "none";
+  }, 2000);
 };
