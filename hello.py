d119a8efd51e578d8788aae12512034408cdedfb
AndreyV <varganov.a.p@gmail.com>
2021-09-10 08:52:28 +0300

    немного тестовых данных

--- hello.py
+++ hello.py
@@ -588,12 +588,12 @@ def stats():
         auth_ok = 1
         user = oauth.fetch_user()
 
+    if app.config["DEV"] == "true":
+        return jsonify({'data': [{"name": "*minemax34700", "active_playtime": "168424", "deaths": "1", "mobs": "0"}, {"name": "xFothis", "active_playtime": "158113", "deaths": "0", "mobs": "0"}, {"name": "Mabotlz", "active_playtime": "153849", "deaths": "0", "mobs": "0"}, {"name": "Spibble", "active_playtime": "150592", "deaths": "0", "mobs": "0"}, {"name": "*OrangeNebula699", "active_playtime": "147768", "deaths": "0", "mobs": "0"}, {"name": "*Dannylpro", "active_playtime": "144836", "deaths": "0", "mobs": "0"}, {"name": "*ScrewyFriend355", "active_playtime": "120177", "deaths": "0", "mobs": "0"}, {"name": "*folgakmg001", "active_playtime": "114871", "deaths": "0", "mobs": "0"}, {"name": "Neorgan1ka", "active_playtime": "88861", "deaths": "0", "mobs": "0"}, {"name": "PegucoH", "active_playtime": "50744", "deaths": "0", "mobs": "0"}, {"name": "zerosc", "active_playtime": "17548", "deaths": "0", "mobs": "0"}]})
+
     response = _sendRequest('getStats', {})
 
     return jsonify({'data': response})
-
-    # return render_template('stats.html', user=user, auth_ok=auth_ok)
-
 
 @app.route("/<page>/")
 def start(page):
