795b5049d0a780ecfa96a00dc265c8519e4c9c71
dalandis <varganov.a.p@gmail.com>
2022-05-07 23:26:41 +0400

    range for farm

--- hello.py
+++ hello.py
@@ -402,6 +402,28 @@ def farm_manager():
                     data["main"].update({key: farm["name"]})
                 else:
                     data["farm"].update({key: farm["name"]})
+		
+		signX = farm["x"][:1]
+                signZ = farm["z"][:1]
+
+                x = farm["x"][1:] if signX == '-' else farm["x"]
+                z = farm["z"][1:] if signZ == '-' else farm["z"]
+
+                addXpositive = [ signX + str(i) if signX == '-' else str(i) for i in range(x, x+6, 1)]
+                addXnegative = [ signX + str(i) if signX == '-' else str(i) for i in range(x, x-6, 1)]
+                addZpositive = [ signZ + str(i) if signZ == '-' else str(i) for i in range(z, z+6, 1)]
+                addZnegative = [ signZ + str(i) if signZ == '-' else str(i) for i in range(z, z-6, 1)]
+
+                for i in range(0,5):
+                    keyPositive = ",".join((addXpositive[i],str(farm["y"]),addZpositive[i]))
+                    keyNegative = ",".join((addXnegative[i],str(farm["y"]),addZnegative[i]))
+
+                    if farm["server"] == 'gmgame':
+                        data["main"].update({keyPositive: farm["name"]})
+                        data["main"].update({keyNegative: farm["name"]})
+                    else:
+                        data["farm"].update({keyPositive: farm["name"]})
+                        data["main"].update({keyNegative: farm["name"]})
 
             response = _sendRequest('reinitFarmManager', data)
 
